97daa4f90f3893ba6850fd0373e41de1c1aac197
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-06-24 15:50:28 +0500

    call api webclient bilan ishlash

--- src/main/java/org/example/springbootadvance/SpringbootadvanceApplication.java
+++ src/main/java/org/example/springbootadvance/SpringbootadvanceApplication.java
@@ -8,6 +8,7 @@ public class SpringbootadvanceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootadvanceApplication.class, args);
-    }
 
+        System.out.println("Mikroserviselar bilan ishlash uchun RestTemplete va WebClientni o'rgandim!");
+    }
 }
