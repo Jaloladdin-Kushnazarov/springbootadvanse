8fd004fed9717690ac1e551f42b73b469fc94c03
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-06-25 21:05:34 +0500

    call api traverson bilan ishlash last....

--- src/main/java/org/example/springbootadvance/SpringbootadvanceApplication.java
+++ src/main/java/org/example/springbootadvance/SpringbootadvanceApplication.java
@@ -9,6 +9,6 @@ public class SpringbootadvanceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootadvanceApplication.class, args);
 
-        System.out.println("Mikroserviselar bilan ishlash uchun RestTemplete va WebClientni o'rgandim!");
+        System.out.println("DB migration tools learning ");
     }
 }
